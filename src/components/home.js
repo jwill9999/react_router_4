b9dcc05e25a06226d7e09257c0b8298316ecd033
Jason Williams <jasonwilliams2308@gmail.com>
2017-07-16 23:55:49 +0100

    Initial commit

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/App.css'
+
+const Home = () => {
+  return (
+    <div className='container'>
+      <h1>Home component</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/App.css'
+
+const Page = () => {
+  return (
+    <div className='container'>
+      <h1>Page component</h1>
+    </div>
+  );
+};
+
+export default Page;
